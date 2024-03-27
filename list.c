cd69dfdc32c03a9148f2de09a49675a383d0e620
Your Name <you@example.com>
2024-03-27 18:24:52 +0000

    new try

--- list.c
+++ list.c
@@ -115,7 +115,23 @@ void * popBack(List * list) {
 }
 
 void * popCurrent(List * list) {
-    return NULL;
+    if(list->current==NULL){
+        return NULL;
+    }
+    void * data =list-current->data;
+    node *nodeToRemove = list->current;
+    if(list->current->prev!=NULL){
+        list->current->prev->next=list->current->next;
+    }else}
+        list->head=list->current->next;
+    }
+    if(list->current->next!=NULL){
+        list->current->next->prev=list->current->prev; 
+    }else{
+        list->tail=list->current->prev;
+    }
+    list->current=list->current->next;
+    free(nodeToRemove);
 }
 
 void cleanList(List * list) {
